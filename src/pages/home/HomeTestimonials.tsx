0de8c3a9089910f74f3fbc87109734989b6f0d5c
Muhammad Seifeldin <muhammad.wds@protonmail.com>
2026-03-19 08:01:21 +0200

    fix: remove cached page folder casing from git index

--- src/pages/home/HomeTestimonials.tsx
+++
@@ -1,8 +0,0 @@
-import type { ReactNode } from "react";
-import TestimonialsSection from "../../components/sections/TestimonialsSection";
-
-const HomeTestimonials = (): ReactNode => {
-	return <TestimonialsSection />;
-};
-
-export default HomeTestimonials;
